afd688e0e57b84c5acec209cfdc1898525f98ae9
Mark Hall <mhall@pentaho.com>
2013-08-09 13:30:16 +1200

    [PDI-10209] MongoDB output step cannot insert an incoming field specified as "JSON" unless it is set as the value of a field in a top-level record; [PDI-10371] Data Integration - Kettle PDI-10371 Leaving "Write concern (w option)" blank should default to write concern 1, but actually defaults to 0. Also snuck in a small logging level change for messages relating to batch commits.

--- src/org/pentaho/mongo/MongoUtils.java
+++ src/org/pentaho/mongo/MongoUtils.java
@@ -243,8 +243,10 @@ public class MongoUtils {
     WriteConcern concern = null;
 
     if (Const.isEmpty(writeConcern) && Const.isEmpty(wTimeout) && !journaled) {
-      concern = new WriteConcern(); // all defaults - timeout 0, journal =
-                                    // false, w = 1
+      // all defaults - timeout 0, journal = false, w = 1
+      concern = new WriteConcern();
+      concern.setWObject(new Integer(1));
+
       if (log != null) {
         log.logBasic(BaseMessages.getString(PKG,
             "MongoUtils.Message.ConfiguringWithDefaultWriteConcern"));
