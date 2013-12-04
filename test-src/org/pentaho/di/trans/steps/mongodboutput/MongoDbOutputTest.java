cbf2284f9933637eb375452b62120777f5838984
Dzmitry <dima.prokopenko@gmail.com>
2013-12-04 12:08:33 +0300

    SP-986 - MongoDB Output Step throws ArrayIndexOutOfBounds when input field is missing.
    junit tests added

--- test-src/org/pentaho/di/trans/steps/mongodboutput/MongoDbOutputTest.java
+++ test-src/org/pentaho/di/trans/steps/mongodboutput/MongoDbOutputTest.java
@@ -8,16 +8,24 @@ import static org.pentaho.di.trans.steps.mongodboutput.MongoDbOutputData.kettleR
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
+
+import org.easymock.Capture;
+import org.easymock.CaptureType;
+import org.easymock.EasyMock;
+import org.easymock.IAnswer;
 import org.junit.Test;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.row.RowMeta;
 import org.pentaho.di.core.row.RowMetaInterface;
 import org.pentaho.di.core.row.ValueMeta;
 import org.pentaho.di.core.row.ValueMetaInterface;
+import org.pentaho.di.core.row.value.ValueMetaString;
 import org.pentaho.di.core.variables.VariableSpace;
 import org.pentaho.di.core.variables.Variables;
 
 import com.mongodb.DBObject;
+
 
 /**
  * Unit tests for MongoDbOutput
@@ -801,7 +809,115 @@ public class MongoDbOutputTest {
         "{ \"field1\" : \"value1\" , \"field2\" : 12 , \"jsonField\" : { \"jsonDocField1\" : \"aval\" , \"jsonDocField2\" : 42}}" );
   }
 
-  public static void main( String[] args ) {
+  /**
+   * PDI-9989. If step configuration contains mongo field names which doesn't have equivalents
+   * in step input row meta - throw exception, with detailed information.
+   * 
+   * (expected mongo field name b1 has no equivalent in input row meta)
+   * @throws KettleException
+   */
+  @Test( expected = KettleException.class )
+  public void testStepIsFailedIfOneOfMongoFieldsNotFound() throws KettleException{
+	MongoDbOutput output = prepareMongoDbOutputMock();
+
+	final String[] metaNames = new String[]{ "a1", "a2", "a3" };
+	String[] mongoNames = new String[]{ "b1", "a2", "a3" };
+	List<MongoDbOutputMeta.MongoField> mongoFields = getMongoFields( mongoNames );
+	RowMetaInterface rmi = getStubRowMetaInterface( metaNames );
+	EasyMock.replay( output );
+	try{
+		output.checkInputFieldsMatch(rmi, mongoFields);
+	} catch ( KettleException e ){
+		String detailedMessage = e.getMessage();
+		Assert.assertTrue("Exception message mentions fields not found:", detailedMessage.contains( "b1" ));
+		throw new KettleException( e );	
+	}
+  }
+  
+  /**
+   * Tests if mongo output configuration contains excessive fields in step input
+   * against mongo output fields, we generate a log record about the fields  
+   * will not be used in mongo output.
+   * @throws KettleException
+   */
+  @Test
+  public void testStepLogSkippedFields() throws KettleException{
+	MongoDbOutput output = prepareMongoDbOutputMock();
+
+	final String[] metaNames = new String[]{ "a1", "a2", "a3" };
+	String[] mongoNames = new String[]{ "a1", "a2" };
+	Capture<String> loggerCapture = new Capture<String>( CaptureType.ALL );
+	output.logBasic( EasyMock.capture( loggerCapture ) );
+	EasyMock.replay( output );
+	RowMetaInterface rmi = getStubRowMetaInterface( metaNames );
+	List<MongoDbOutputMeta.MongoField> mongoFields = getMongoFields( mongoNames );
+	
+	output.checkInputFieldsMatch(rmi, mongoFields);	  
+	List<String> logRecords = loggerCapture.getValues();
+
+	Assert.assertEquals("We have one log record generated", 1, logRecords.size() );
+	Assert.assertTrue("We have a log record mentions that 'a3' field will not be used.", 
+			logRecords.get( 0 ).contains( "a3" ));
+  }
+  
+  /**
+   * Tests if none mongo output fields exists - step is failed.
+   * @throws KettleException
+   */
+  @Test( expected = KettleException.class )
+  public void testStepFailsIfNoMongoFieldsFound() throws KettleException{
+	MongoDbOutput output = prepareMongoDbOutputMock();
+
+	final String[] metaNames = new String[]{ "a1", "a2" };
+	String[] mongoNames = new String[]{  };
+	EasyMock.replay( output );
+	RowMetaInterface rmi = getStubRowMetaInterface( metaNames );
+	List<MongoDbOutputMeta.MongoField> mongoFields = getMongoFields( mongoNames );
+	output.checkInputFieldsMatch(rmi, mongoFields);
+  }
+  
+
+  private MongoDbOutput prepareMongoDbOutputMock(){  
+	  MongoDbOutput output = EasyMock.createNiceMock( MongoDbOutput.class );
+	  EasyMock.expect( output.environmentSubstitute( EasyMock.anyObject( String.class ) ) )
+			  .andAnswer(new IAnswer<String>(){
+		@Override
+		public String answer() throws Throwable {
+			Object[] args = EasyMock.getCurrentArguments();
+			String ret = String.valueOf( args[0] );
+			return ret;
+		}
+	  } ).anyTimes();
+	  return output;
+  }
+  
+  private RowMetaInterface getStubRowMetaInterface( final String[] metaNames ){
+	  RowMetaInterface rmi = EasyMock.createNiceMock( RowMetaInterface.class );
+	  EasyMock.expect( rmi.getFieldNames() ).andReturn( metaNames ).anyTimes();
+	  EasyMock.expect( rmi.size() ).andReturn( metaNames.length ).anyTimes();
+	  EasyMock.expect( rmi.getValueMeta( EasyMock.anyInt() )).andAnswer( new IAnswer<ValueMetaInterface>(){
+		@Override
+		public ValueMetaInterface answer() throws Throwable {
+			Object[] args = EasyMock.getCurrentArguments();
+			int i = Integer.class.cast( args[0] );
+			ValueMetaInterface ret = new ValueMetaString( metaNames[i] );
+			return ret;
+		}} ).anyTimes();
+	  EasyMock.replay( rmi );	  
+	  return rmi;
+  }
+  
+  private List<MongoDbOutputMeta.MongoField> getMongoFields( String[] names ){
+	  List<MongoDbOutputMeta.MongoField> ret = new ArrayList<MongoDbOutputMeta.MongoField>( names.length );
+	  for ( int i=0; i<names.length; i++ ){
+		  MongoDbOutputMeta.MongoField field = new MongoDbOutputMeta.MongoField();
+		  field.m_incomingFieldName = names[i];
+		  ret.add( field );
+	  }
+	  return ret;
+  }
+
+  public static void main(String[] args) {
     try {
       MongoDbOutputTest test = new MongoDbOutputTest();
       test.testInsertKettleFieldThatContainsJsonIntoTopLevelRecord();
