08f227ae31be44458139f686e4d9a9e4d81fb7d8
Tatsiana Kasiankova <tatsiana_kasiankova@epam.com>
2014-06-13 17:55:18 +0300

    [PDI-12155]: Error when saving a transformation with MongoDB Output step into Kettle Database Repository
    There is a fix.

--- src/org/pentaho/di/trans/steps/mongodboutput/MongoDbOutputMeta.java
+++ src/org/pentaho/di/trans/steps/mongodboutput/MongoDbOutputMeta.java
@@ -793,13 +793,7 @@ public class MongoDbOutputMeta extends MongoDbMeta implements StepMetaInterface 
   }
   
   @Override
-  public void saveRep( Repository rep, IMetaStore metaStore, ObjectId id_transformation, ObjectId id_step )
-    throws KettleException {
-    saveRep( rep, id_transformation, id_step );
-  }
-
-  @Override
-  public void saveRep( Repository rep, ObjectId id_transformation, ObjectId id_step ) throws KettleException {
+  public void saveRep( Repository rep, IMetaStore metaStore, ObjectId id_transformation, ObjectId id_step ) throws KettleException {
     if ( !Const.isEmpty( getHostnames() ) ) {
       rep.saveStepAttribute( id_transformation, id_step, 0, "mongo_host", //$NON-NLS-1$
           getHostnames() );
