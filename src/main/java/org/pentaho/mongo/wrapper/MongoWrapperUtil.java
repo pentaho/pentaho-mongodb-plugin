2b9e43018e065d26f81debb64b8659d13c98ea48
Aleksandr Kozlov <Aleksandr_Kozlov1@epam.com>
2016-02-17 10:50:08 +0400

    [SP-2491] Backport of PDI-14881 - MongoDB Input step does not accept username/password environment variables (6.0 Suite) - environmentSubstitute for username and password was added

--- src/main/java/org/pentaho/mongo/wrapper/MongoWrapperUtil.java
+++ src/main/java/org/pentaho/mongo/wrapper/MongoWrapperUtil.java
@@ -49,8 +49,8 @@ public class MongoWrapperUtil {
     setIfNotNullOrEmpty( propertiesBuilder, MongoProp.JOURNALED, Boolean.toString( mongoDbMeta.getJournal() ) );
     setIfNotNullOrEmpty( propertiesBuilder, MongoProp.USE_ALL_REPLICA_SET_MEMBERS,
         Boolean.toString( mongoDbMeta.getUseAllReplicaSetMembers() ) );
-    setIfNotNullOrEmpty( propertiesBuilder, MongoProp.USERNAME, mongoDbMeta.getAuthenticationUser() );
-    setIfNotNullOrEmpty( propertiesBuilder, MongoProp.PASSWORD, mongoDbMeta.getAuthenticationPassword() );
+    setIfNotNullOrEmpty( propertiesBuilder, MongoProp.USERNAME, vars.environmentSubstitute( mongoDbMeta.getAuthenticationUser() ) );
+    setIfNotNullOrEmpty( propertiesBuilder, MongoProp.PASSWORD, vars.environmentSubstitute( mongoDbMeta.getAuthenticationPassword() ) );
     setIfNotNullOrEmpty( propertiesBuilder, MongoProp.USE_KERBEROS,
         Boolean.toString( mongoDbMeta.getUseKerberosAuthentication() ) );
     if ( mongoDbMeta.getReadPrefTagSets() != null ) {
