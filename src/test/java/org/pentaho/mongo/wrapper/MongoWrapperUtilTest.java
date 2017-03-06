d07df1166fe012e7fccff5db22f4a6ffc7dd800c
PashkovichAndrei <andrei_pashkovich@epam.com>
2017-03-06 19:18:12 +0300

    [PDI-16022]-Test transformation fails with encrypted password in a parameter for "MongoDB Input" and "MongoDB Output" Steps * Added decryption for encrypted passwords

--- src/test/java/org/pentaho/mongo/wrapper/MongoWrapperUtilTest.java
+++ src/test/java/org/pentaho/mongo/wrapper/MongoWrapperUtilTest.java
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
+ * Copyright 2010 - 2017 Pentaho Corporation.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,11 +20,18 @@ package org.pentaho.mongo.wrapper;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.pentaho.di.core.Const;
+import org.pentaho.di.core.encryption.Encr;
+import org.pentaho.di.core.encryption.TwoWayPasswordEncoderPluginType;
+import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.logging.LogChannelInterface;
+import org.pentaho.di.core.plugins.PluginRegistry;
+import org.pentaho.di.core.util.EnvUtil;
 import org.pentaho.di.core.variables.VariableSpace;
 import org.pentaho.di.core.variables.Variables;
 import org.pentaho.di.trans.steps.mongodb.MongoDbMeta;
 import org.pentaho.di.trans.steps.mongodbinput.MongoDbInputMeta;
+import org.pentaho.di.trans.steps.mongodboutput.MongoDbOutputMeta;
 import org.pentaho.mongo.MongoDbException;
 import org.pentaho.mongo.MongoProp;
 import org.pentaho.mongo.MongoProperties;
@@ -42,6 +49,7 @@ import static org.mockito.Mockito.when;
 public class MongoWrapperUtilTest {
   private static final String SOCKET_TIMEOUT = "mongoDbSocketTimeout";
   private static final String CONNECTION_TIMEOUT = "mongoDbConnectionTimeout";
+  private static final String PASSWORD = "password";
 
   private MongoWrapperClientFactory cachedFactory;
   private MongoWrapperClientFactory mockFactory;
@@ -85,7 +93,7 @@ public class MongoWrapperUtilTest {
     MongoDbMeta input = new MongoDbInputMeta();
     setSocetAndConnectionTimeouts( input, "${" + CONNECTION_TIMEOUT + "}", "${" + SOCKET_TIMEOUT + "}" );
 
-    MongoDbMeta output = new MongoDbInputMeta();
+    MongoDbMeta output = new MongoDbOutputMeta();
     setSocetAndConnectionTimeouts( output, "${" + CONNECTION_TIMEOUT + "}", "${" + SOCKET_TIMEOUT + "}" );
 
     VariableSpace vars = new Variables();
@@ -99,13 +107,60 @@ public class MongoWrapperUtilTest {
     checkProps( outProps, "200", "500" );
   }
 
+  @Test public void testPropertiesBuilderEncrPassword() throws KettleException {
+    final String pass = "pass";
+    testPropertiesBuilderForPassword( true, pass );
+    testPropertiesBuilderForPassword( false, pass );
+  }
+
+  private void testPropertiesBuilderForPassword( boolean isEncrypted, String password ) throws KettleException {
+    MongoDbMeta input = new MongoDbInputMeta();
+    setPassword( input, "${" + PASSWORD + "}" );
+
+    MongoDbMeta output = new MongoDbOutputMeta();
+    setPassword( output, "${" + PASSWORD + "}" );
+
+    VariableSpace vars = new Variables();
+
+    initEncryptor();
+
+    String value;
+    if ( isEncrypted ) {
+      value = Encr.encryptPasswordIfNotUsingVariables( password );
+    } else {
+      value = password;
+    }
+    vars.setVariable( PASSWORD, value );
+
+    MongoProperties inProps = MongoWrapperUtil.createPropertiesBuilder( input, vars ).build();
+    MongoProperties outProps = MongoWrapperUtil.createPropertiesBuilder( output, vars ).build();
+
+    checkPass( inProps, password );
+    checkPass( outProps, password );
+  }
+
+  private void initEncryptor() throws KettleException {
+    PluginRegistry.addPluginType( TwoWayPasswordEncoderPluginType.getInstance() );
+    PluginRegistry.init( true );
+    String passwordEncoderPluginID = Const.NVL( EnvUtil.getSystemProperty( Const.KETTLE_PASSWORD_ENCODER_PLUGIN ), "Kettle" );
+    Encr.init( passwordEncoderPluginID );
+  }
+
   private void setSocetAndConnectionTimeouts(MongoDbMeta meta, String connection, String session) {
     meta.setConnectTimeout( connection );
     meta.setSocketTimeout( session );
   }
 
-  private void checkProps(MongoProperties props, String cTimeout, String sTimeout) {
+  private void setPassword( MongoDbMeta meta, String password ) {
+    meta.setAuthenticationPassword( password );
+  }
+
+  private void checkProps( MongoProperties props, String cTimeout, String sTimeout ) {
     assertEquals( cTimeout , props.get( MongoProp.connectTimeout ) );
     assertEquals( sTimeout , props.get( MongoProp.socketTimeout ) );
+  }
+
+  private void checkPass( MongoProperties props, String password ) {
+    assertEquals( password, props.get( MongoProp.PASSWORD ) );
   }
 }
