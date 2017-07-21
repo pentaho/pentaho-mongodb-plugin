10510aba45372a02761c738b4d0b60af2d104faa
Artsiom Yurhevich <Artsiom_Yurhevich@epam.com>
2017-07-21 19:26:31 +0300

    [SP-3657] - Backport of PDI-14460 - Mongo DB Output step does not output/forward rows to a next step

--- src/test/java/org/pentaho/di/trans/steps/mongodboutput/MongoDbOutputTest.java
+++ src/test/java/org/pentaho/di/trans/steps/mongodboutput/MongoDbOutputTest.java
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2010 - 2015 Pentaho Corporation.  All rights reserved.
+ * Copyright 2010 - 2017 Pentaho Corporation.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleStepException;
 import org.pentaho.di.core.plugins.PluginRegistry;
 import org.pentaho.di.core.row.RowMeta;
 import org.pentaho.di.core.row.RowMetaInterface;
@@ -63,12 +64,21 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.anyList;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doThrow;
 import static org.pentaho.di.trans.steps.mongodboutput.MongoDbOutputData.kettleRowToMongo;
 
 /**
@@ -85,6 +95,7 @@ public class MongoDbOutputTest extends BaseMongoDbStepTest {
   private MongoDbOutput dbOutput;
   RowMeta rowMeta = new RowMeta();
   Object[] rowData;
+  List<Object[]> outputRowData;
 
   private List<MongoDbOutputMeta.MongoField> mongoFields = new ArrayList<MongoDbOutputMeta.MongoField>();
 
@@ -98,6 +109,11 @@ public class MongoDbOutputTest extends BaseMongoDbStepTest {
     dbOutput = new MongoDbOutput( stepMeta, stepDataInterace, 1, transMeta, trans ) {
       public Object[] getRow() {
         return rowData;
+      }
+
+      @Override
+      public void putRow( RowMetaInterface rowMeta, Object[] row ) throws KettleStepException {
+        outputRowData.add( row );
       }
 
       public RowMetaInterface getInputRowMeta() {
@@ -829,6 +845,7 @@ public class MongoDbOutputTest extends BaseMongoDbStepTest {
     // no input has been defined, so should gracefully finish and clean up.
     assertFalse( dbOutput.processRow( stepMetaInterface, stepDataInterace ) );
     verify( mongoClientWrapper ).dispose();
+    assertEquals( 0, dbOutput.getLinesOutput() );
   }
 
   @Test public void testProcessRowWithInput() throws Exception {
@@ -838,8 +855,20 @@ public class MongoDbOutputTest extends BaseMongoDbStepTest {
     assertTrue( dbOutput.processRow( stepMetaInterface, stepDataInterace ) );
   }
 
+  @Test public void testProcessRowPassRowsToNextStep() throws Exception {
+    setupReturns();
+    setupRowMeta();
+    dbOutput.init( stepMetaInterface, stepDataInterace );
+    assertTrue( dbOutput.processRow( stepMetaInterface, stepDataInterace ) );
+    assertTrue( dbOutput.processRow( stepMetaInterface, stepDataInterace ) );
+    assertTrue( dbOutput.processRow( stepMetaInterface, stepDataInterace ) );
+    assertEquals( 3, outputRowData.size() );
+    assertEquals( 3, dbOutput.getLinesOutput() );
+  }
+
   private void setupRowMeta() {
     rowData = new Object[] { "foo", "bar", "baz" };
+    outputRowData = new ArrayList<Object[]>();
     rowMeta.addValueMeta( new ValueMetaString( "foo" ) );
     rowMeta.addValueMeta( new ValueMetaString( "bar" ) );
     rowMeta.addValueMeta( new ValueMetaString( "baz" ) );
